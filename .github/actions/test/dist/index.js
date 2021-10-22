095b48599dcb5376faf7e67c5f76368dff627e0f
Marcin Musiał <marcin@everydayiplay.net>
2021-10-22 15:29:23 +0200

    www

--- .github/actions/test/dist/index.js
+++ .github/actions/test/dist/index.js
@@ -8466,13 +8466,21 @@ async function main(payload) {
 
         const merge_commit_sha = pull_request.merge_commit_sha;
     
-        console.log(`The event payload: ${payload_json}`);
+        //console.log(`The event payload: ${payload_json}`);
         console.log(`Merge Commit SHA: ${merge_commit_sha}`);
         
         const repo_token = core.getInput('repo-token');
         console.log(`Token: ${repo_token}`);
         const octokit = github.getOctokit(repo_token);
 
+        const commit_info = await octokit.rest.commits.get({
+            owner: "mmusial",
+            repo: "dcst",
+            ref: merge_commit_sha
+          });
+        const commit_info_json = JSON.stringify(commit_info, undefined, 2);
+        console.log(`${commit_info_json}`);
+        
     } catch (error) {
         core.setFailed(error.message);
     }
