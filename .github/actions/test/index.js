ec026853b31d68f491e9ca26113b6ed2137ad5e6
Marcin Musiał <marcin@everydayiplay.net>
2021-10-22 15:00:14 +0200

    check for property test

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -2,12 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    const payload = github.context.payload;
+    const payload_json = JSON.stringify(payload, undefined, 2)
 
-    const pull_request = github.context.payload.pull_request;
+    
+    if ('pull_request' in payload) {
+        core.setFailed(error.message);
+    }
+
+    const pull_request = payload.pull_request;
     const merge_commit_sha = pull_request.merge_commit_sha;
 
-    console.log(`The event payload: ${payload}`);
+    console.log(`The event payload: ${payload_json}`);
     console.log(`Merge Commit SHA: ${merge_commit_sha}`);
 } catch (error) {
     core.setFailed(error.message);
