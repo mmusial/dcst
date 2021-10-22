d38ff4b1a98990dd87717663a64c4f1c5eb14a9d
Marcin Musiał <marcin@everydayiplay.net>
2021-10-22 15:05:19 +0200

    aaa

--- .github/actions/test/dist/index.js
+++ .github/actions/test/dist/index.js
@@ -8445,23 +8445,31 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(611);
 const github = __nccwpck_require__(9117);
 
-try {
-    const payload = github.context.payload;
-    const payload_json = JSON.stringify(payload, undefined, 2)
 
+
+
+
+function main(payload) {
+    try {
+        const payload_json = JSON.stringify(payload, undefined, 2)    
+        
+        if ('pull_request' in payload) {
+            core.setFailed("No 'pull_request' in context payload");
+            return;
+        }
     
-    if ('pull_request' in payload) {
-        core.setFailed("No 'pull_request' in context payload");
+        const pull_request = payload.pull_request;
+        const merge_commit_sha = pull_request.merge_commit_sha;
+    
+        console.log(`The event payload: ${payload_json}`);
+        console.log(`Merge Commit SHA: ${merge_commit_sha}`);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    const pull_request = payload.pull_request;
-    const merge_commit_sha = pull_request.merge_commit_sha;
-
-    console.log(`The event payload: ${payload_json}`);
-    console.log(`Merge Commit SHA: ${merge_commit_sha}`);
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+
+main(github.context.payload);
 })();
 
 module.exports = __webpack_exports__;
